59812ff18f7149b70d128d2c428dc6eea06366eb
Jan Van Balen <janvanbalen@gmail.com>
2023-08-18 23:52:24 +0200

    Update demo script doc string

--- demo.py
+++ demo.py
@@ -1,8 +1,14 @@
 """
-An effect chain with randomly evolving effect parameters, and feedback
+This example script instantiates a MIDI effect chain in which:
+- input is read from a MIDI file
+- various simple effects are applied including pitch shift, velocity changes, and delay
+- the output is fed back to the input to create a feedback loop (!)
+  (once the end of the file is reached)
+- effect parameters evolve randomly on each iteration
+- results are written to a file (or, if preferred, sent to a MIDI out port)
 
 To write to a file:
-  python -m midifx.demo --in-path data/scale.mid --out-dir generated --max-out 1000
+  python -m midifx.demo --in-path data/scale.mid --max-out 1000
 
 To send over MIDI:
   python -m midifx.demo --in-path data/scale.mid --send "MidiFX out"
@@ -21,15 +27,15 @@ PHRASE_END = ControlChange(start=None, number=ControlNumbers.END_MESSAGE, value=
 
 
 parser = ArgumentParser()
+parser.add_argument("--in-path", type=str, required=True)
 parser.add_argument("--send", type=str, default=None)
-parser.add_argument("--in-path", type=str, default=None)
-parser.add_argument("--out-dir", type=str, default=None)
 parser.add_argument("--max-out", type=int, default=None)
 parser.add_argument("--debug", "-d", action="store_true")
 args = parser.parse_args()
 configure_logging(debug=args.debug)
 
 modules = [
+    ReadMIDI(path=args.in_path, start_message=PHRASE_START, end_message=PHRASE_END),
     PitchShift(
         amount=RandomParameter(
             initial_value=0,
@@ -57,12 +63,11 @@ modules = [
         )
     ),
     BufferDelay(control_message=PHRASE_END),
-    ReadMIDI(path=args.in_path, start_message=PHRASE_START, end_message=PHRASE_END),
 ]
 if args.send:
     modules.append(SendMIDI(name=f"{args.send}"))
-if args.out_dir:
+else:
     modules.append(MIDILogger(override_channel=1, max_messages=args.max_out))
 
-chain = Chain(*modules, log_dir=args.out_dir, loop=True)
+chain = Chain(*modules, log_dir=".", loop=True)
 chain.run()
