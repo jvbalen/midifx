46e21a28ffbbaee5b255432c81f5771c4a0e71bd
Jan Van Balen <janvanbalen@gmail.com>
2023-08-18 23:22:25 +0200

    Remove unused import, please flake8

--- midifx/note.py
+++ midifx/note.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import logging
 import math
 from collections import defaultdict
-from dataclasses import dataclass, replace
+from dataclasses import dataclass
 from typing import Iterable, Optional, Tuple
 
 
